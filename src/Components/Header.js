299ffaff56a41c8ff40149cf818594a941294603
ArianBer <66421511+ArianBer@users.noreply.github.com>
2024-04-29 21:25:03 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,7 +17,6 @@ const Header = () => {
                             <li><a className="smoothscroll"  href="#about" title="">About</a></li>
                             <li><a className="smoothscroll"  href="#resume" title="">Resume</a></li>
                             <li><a className="smoothscroll"  href="#portfolio" title="">Portfolio</a></li>
-                            <li><a className="smoothscroll"  href="#services" title="">Services</a></li>					
                             <li><a className="smoothscroll"  href="#contact" title="">Contact</a></li>	
                             <li><a href="styles.html" title="">Style Demo</a></li>				
                         </ul>
